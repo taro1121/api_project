fc3ba50552d02f31a49ba743dbbb37bf8d239c7b
taro1121 <shintaro.eiyo@gmail.com>
2020-04-14 02:11:04 -0400

    added bootstrap

--- js/app.js
+++ js/app.js
@@ -58,6 +58,29 @@ $(function() {
     sortByRelevance(copyKeyword());
   });
 
+
+// Gets the video src from the data-src on each button
+  var $videoSrc;
+  $('.video-btn').click(function() {
+    $videoSrc = $(this).data("data-src");
+  });
+  console.log("$videoSrc is: " + $videoSrc);
+
+// when the modal is opened autoplay it
+  $('#myModal').on('shown.bs.modal', function (e) {
+
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    // $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
+    $("#video").attr('src',$videoSrc );
+
+  })
+
+// stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc);
+  })
+
 });
 
 
@@ -164,9 +187,9 @@ function resultsLoop(data){
 
   $("#results").append(`
     <li>
-      <a target="_blank" href="https://www.youtube.com/watch?v=${videoid}">
-      <img src="${thumb}" alt="" class="thumb">
-      </a>
+      <button type="button" class="btn btn-primary video-btn" data-toggle="modal" data-src="https://www.youtube.com/embed/${videoid}" data-target="#myModal">
+        <img src="${thumb}" alt="">
+      </button>
       <div class= "details">
         <h4>${channel_title}</h4>
         <h4>${title}</h4>
